8cf35268d03540cefeb0958e0379b274be0873d9
Floflote <florent13a@gmail.com>
2023-04-05 16:13:10 +0200

    Add-activebutton-filter

--- Website/Design/JS/website.js
+++ Website/Design/JS/website.js
@@ -98,6 +98,16 @@ $(document).on("click", "body", function () {
   }
 });
 
+var btnContainer = document.getElementById("btnContainer");
+var btns = btnContainer.getElementsByClassName("product_category_name");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active_category");
+    current[0].className = current[0].className.replace(" active_category", "");
+    this.className += " active_category";
+  });
+}
+
 //Resa
 let field = document.querySelector("#date");
 // Handle date changes
